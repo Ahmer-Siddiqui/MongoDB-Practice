6bac125d0aaf2f7d632ccf26a91753d5328cf226
Ahmer-Siddiqui <siddiquiahmer970@gmail.com>
2023-06-15 23:04:01 +0500

    insert data

---
+++ insert.js
@@ -0,0 +1,12 @@
+const dbConnect = require('./mongodb')
+
+const insert = async ()=>{
+    const db = await dbConnect();
+    const result = db.insertMany(
+        [
+            {name:'max 4', brand:'micromax',price:220,category:'mobile'}
+        ]
+    )
+}
+
+insert();
